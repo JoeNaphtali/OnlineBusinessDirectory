b7d889a16caef93a1ba456bdc3be88de037e9ba2
Joseph Wamulume <jaywams@outlook.com>
2020-06-13 11:41:47 +0200

    Initial

---
+++ index.php
@@ -0,0 +1,29 @@
+<html lang="en">
+
+    <head>
+		<title>Home | Find Us</title>
+        <meta charset="utf-8">
+		<meta content="width=device-width, initial-scale=1" name="viewport"/>
+		<!-- Font Awesome -->
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+		<!-- Material Icons -->
+		<link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet">
+		<!-- Google Fonts -->
+		<link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+		<!-- Bootstrap core CSS -->
+		<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+		<!-- JQuery -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <!-- Custom Fonts -->
+        <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<!-- Bootstrap tooltips -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+		<!-- Bootstrap core JavaScript -->
+		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
+		<!-- Local Stylesheet -->
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<!-- Local Script -->
+		<script type="text/javascript" src="js/script.js"></script>
+	</head>
+
+</html>
