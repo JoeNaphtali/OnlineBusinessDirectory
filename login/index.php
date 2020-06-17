87f48aef32fc760843bfe51a1ae2809727562859
Joseph Wamulume <josephwamulume@gmail.com>
2020-06-17 14:07:23 +0200

    Added separate login and registration pages

--- login/index.php
+++ login/index.php
@@ -2,7 +2,7 @@
 
     <head>
 
-		<title>Home | FindUs</title>
+		<title>Login | FindUs</title>
         <meta charset="utf-8">
 		<meta content="width=device-width, initial-scale=1" name="viewport"/>
 		<!-- Font Awesome -->
@@ -22,9 +22,9 @@
 		<!-- Bootstrap core JavaScript -->
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.4.1/js/bootstrap.min.js"></script>
 		<!-- Local Stylesheet -->
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="../css/style.css">
 		<!-- Local Script -->
-		<script type="text/javascript" src="js/script.js"></script>
+		<script type="text/javascript" src="../js/script.js"></script>
 
 	</head>
 
@@ -33,66 +33,77 @@
 		<!-- Navigation Bar -->
 
 		<nav class="navbar navbar-light navbar-expand-lg bg-faded justify-content-center static-top">
-			<a href="index.php" class="navbar-brand d-flex w-50 mr-auto">FindUs</a>
+			<a href="../index.php" class="navbar-brand d-flex w-50 mr-auto">FindUs</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 			<div class="navbar-collapse collapse w-100" id="navbar">
 				<ul class="navbar-nav w-100 justify-content-center">
-					<li class="nav-item current">
-						<a class="nav-link" href="index.php">Home</a>
+					<li class="nav-item">
+						<a class="nav-link" href="../index.php">Home</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="businesses/index.php">Businesses</a>
+						<a class="nav-link" href="../businesses/index.php">Businesses</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="deals/index.php">Deals</a>
+						<a class="nav-link" href="../deals/index.php">Deals</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="events/index.php">Events</a>
+						<a class="nav-link" href="../events/index.php">Events</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link" href="blog/index.php">Blog</a>
+						<a class="nav-link" href="../blog/index.php">Blog</a>
 					</li>
 				</ul>
 				<div class="nav navbar-nav ml-auto w-100 justify-content-end">
 					<a class="btn btn-addlisting" href="#">Add Listing</a>
-					<a class="btn btn-login" href="login/index.php">Login</a>
+					<a class="btn btn-login" href="../login/index.php">Login</a>
 				</div>
 			</div>
 		</nav>
 
 		<!-- /.Navigation Bar -->
 
-		<!-- Header -->
+        <!-- Login Form -->
 
-		<header class="masthead">
-			<div class="container h-100">
-				<div class="row h-100 align-items-center">
-				<div class="col-12 text-center">
-					<h1 class="font-weight-light">Connect with great local businesses</h1>
-					<p class="lead">Use FindUs to locate businesses near you</p>
-					<form class="">
-						<div class="form-row">
-							<div class="form-group col-md-5">
-								<input class="form-control" name="search" type="text" 
-								placeholder="What are you looking for?"/>
-							</div>
-							<div class="form-group col-md-5">
-								<input class="form-control" name="search" type="text" 
-								placeholder="Where?"/>
-							</div>
-							<div class="form-group col-md-2">
-								<button class="btn btn-search">Search</button>
-							</div>								
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="form col-lg-4 bg-light px-0 shadow" style="margin-top: 112px;">
+					<form class="p-4 bg-white" action="../includes/login.inc.php" method="post">
+						<div class="form-group">
+							<label>Email</label>
+							<input class="form-control" type="email" name="email" required>
+						</div>
+						<div class="form-group">
+							<label>Password</label>
+							<input class="form-control" type="password" name="password" required>
+						</div>
+						<div class="form-group">
+							<a href="#">Forgot your password?</a>
+						</div>
+						<div class="form-group">
+							<button class="btn btn-block btn-submit" type="submit" name="login-submit">Login</button>
+						</div>
+						<div class="form-group">
+							<small>By logging in, you agree to our <a href="#">Terms of Service</a> and <a href="#">Privacy Policy</a>.</small>
+						</div>
+						<div class="form-group text-center">
+							Or continue with:
+						</div>
+						<div class="form-group text-center">
+							<a href="#"><i class="fab fa-facebook fa-2x"></i></a>
+							<a href="#"><i class="fab fa-google fa-2x"></i></a>
+							<a href="#"><i class="fab fa-twitter fa-2x"></i></a>
+						</div>
+						<div class="form-group">
+							New to FindUs? <a href="../register/index.php">Register</a>
 						</div>
 					</form>
-				</div>
-				</div>
-			</div>
-		</header>
+                </div>
+            </div>
+        </div>
 
-		<!-- /.Header -->
+        <!-- /.Login Form -->
 
 	</body>
 
