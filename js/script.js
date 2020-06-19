666054c64cd352faddda566eaa777427ee9a3a5d
Joseph Wamulume <josephwamulume@gmail.com>
2020-06-19 13:57:57 +0200

    Javascript to toggle navbar is mobile view added

--- js/script.js
+++ js/script.js
@@ -1,18 +1,22 @@
 $(document).ready(function () {
-
+    
     $(document).click(function (event) {
-        var click = $(event.target);
-        var _open = $(".navbar-collapse").hasClass("show");
-        if (_open === true && !click.hasClass("hamburger-icon")) {
-            $(".hamburger-icon").click();
+        if ($(event.target).parents(".navbar-collapse").length < 1) {
+            var click = $(event.target);
+            var _open = $(".navbar-collapse").hasClass("show");
+            if (_open === true && !click.hasClass("hamburger-icon")) {      
+                $(".hamburger-icon").click();
+            }
         }
     });
 
     $(document).click(function (event) {
-        var click = $(event.target);
-        var _open = $(".search-collapse").hasClass("show");
-        if (_open === true && !click.hasClass("search-icon")) {
-            $(".search-icon").click();
+        if ($(event.target).parents(".search-collapse").length < 1) {
+            var click = $(event.target);
+            var _open = $(".search-collapse").hasClass("show");
+            if (_open === true && !click.hasClass("search-icon")) {      
+                $(".search-icon").click();
+            }
         }
     });
     
